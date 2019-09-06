37105abb10474cd620dee8708a3d210d9d91e0d5
oneNutW0nder <scb5436@rit.edu>
2019-09-06 18:27:49 -0400

    Making travis work

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_helloWorld():
-    response = requests.get("http://localhost:8080", verify=false)
+    response = requests.get("http://localhost:80")
 
     assert "Hello World -- pytesting" in response.text
