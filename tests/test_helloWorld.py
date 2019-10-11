785730106b1fef1ac7110c577e9e19a68bc17863
oneNutW0nder <scb5436@rit.edu>
2019-10-11 17:46:29 -0400

    Changed url to match updates

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_helloWorld():
-    response = requests.get("https://localhost:8080/hello.html", verify=False)
+    response = requests.get("https://localhost/hello.html", verify=False)
 
     assert "Hello World -- pytesting" in response.text
