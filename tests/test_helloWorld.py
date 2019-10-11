ec1b5c13ac5c0b14f77d1f1c1568d08f5af5bf08
oneNutW0nder <scb5436@rit.edu>
2019-10-11 10:39:08 -0400

    Update test to work on new page

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_helloWorld():
-    response = requests.get("http://localhost:80")
+    response = requests.get("http://localhost:8080/hello.html")
 
     assert "Hello World -- pytesting" in response.text
