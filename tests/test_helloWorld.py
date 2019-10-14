ae6606db9d7c97722cec85f458a58903a8f62cc9
oneNutW0nder <scb5436@rit.edu>
2019-10-13 21:30:23 -0400

    back to normal

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -4,7 +4,4 @@ import requests
 def test_helloWorld():
     response = requests.get("http://localhost/", verify=False)
 
-    try:
-        assert "Login" in response.text
-    except ConnectionError:
-        assert True
+    assert "Login" in response.text
