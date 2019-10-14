c0267e56329d527580fc199f55d25db068b46635
oneNutW0nder <scb5436@rit.edu>
2019-10-13 21:16:03 -0400

    plz good lord

--- tests/test_helloWorld.py
+++ tests/test_helloWorld.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_helloWorld():
-    response = requests.get("https://localhost/hello", verify=False)
+    response = requests.get("http://localhost:80/", verify=False)
 
-    assert "Hello World -- pytesting" in response.text
+    assert "Login" in response.text
