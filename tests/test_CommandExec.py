7c969ee32ad6fa4b507f9fc67a6bf4c1ed66049d
Rob <15719308+rvblakely5@users.noreply.github.com>
2019-11-22 20:37:20 -0500

    Account Fixed & Command Test Case

--- tests/test_CommandExec.py
+++ tests/test_CommandExec.py
@@ -1,17 +1,22 @@
 import requests
 
+#Test case for command execution
 def test_CommandExec():
+    #Create User
+    resp = requests.post("https://localhost/register", data={"username": "test_exec", "password": "exec_password", "confirmpassword": "exec_password"}, verify=False)
+    
     # Login as valid user -- True
     s = requests.session()
-    resp = s.post("https://localhost/login", data={"username": "test_user", "password": "test_password"}, verify=False)
+    resp = s.post("https://localhost/login", data={"username": "test_exec", "password": "exec_password"}, verify=False)
 
     # Check to see if login was successful by hitting main page
     resp = s.get("https://localhost/", verify=False)
     assert "Videos" in resp.text
 
-    # Send post to /download endpoint
-    data = {"filename": "testDownload.mp4", "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4; [command]"}
+    # Send post to /download endpoint with command
+    data = {"filename": "testDownload.mp4", "url": "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4; echo '<p>comex</p>' >> hello.html}
     resp = s.post("https://localhost/download", data=data, verify=False)
-    assert "successfully" in resp.text
     
-    #To-Do: Write test case for command execution
+    response = requests.get("http://localhost", verify=False)
+    assert "comex" in response.text
+            
