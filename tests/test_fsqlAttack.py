7dc70c41a64589a7a11603e2346037a1709ca0f7
oneNutW0nder <scb5436@rit.edu>
2019-11-24 19:43:58 -0500

    sqli classic test and fixes

--- tests/test_fsqlAttack.py
+++ tests/test_fsqlAttack.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def test_sql_blind_statement():
+def test_sql_blind():
     """
     test a true condition for an SQL attack
     """
@@ -11,3 +11,19 @@ def test_sql_blind_statement():
         assert True
     else:
         assert False
+
+def test_sql_classic():
+    """
+    Tests classic sql injection attack on the search bar of / route
+    """
+
+    # Login as valid user -- True
+    s = requests.session()
+    resp = s.post("https://localhost/login", data={"username": "test_user", "password": "test_password"}, verify=False)
+
+    # Check to see if login was successful by hitting main page
+    resp = s.get("https://localhost/", verify=False)
+    assert "Videos" in resp.text
+
+    resp = s.get("https://localhost/?search_string=%27+UNION+SELECT+*+FROM+users%3B+--+", verify=False)
+    assert "test_user" in resp.text
